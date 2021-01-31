a8e469efb0ebfc3f739b608f060c9a2245b46acd
Kish Dubey <dkish2000@gmail.com>
2021-01-31 01:12:00 -0500

    reogranized for flask

--- hippo/app.py
+++ hippo/app.py
@@ -1,33 +1,53 @@
-import numpy as np
+from flask import Flask, render_template
+
 import keras
 from  keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-import os
-from keras.utils import to_categorical
-from keras.models import Sequential
-from keras.layers import LSTM,Bidirectional,Dense,Embedding,Dropout
 import pickle
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("hippo.html")
 
 MODEL_PATH = "classification_model/model.h5"
 TOKENIZER_PATH =  "classification_model/tokenizer.pickle"
+tokenizer, model = _setup_models(TOKENIZER_PATH, MODEL_PATH)
 
-with open(TOKENIZER_PATH, 'rb') as handle:
-    tokenizer = pickle.load(handle)
+def _setup_models(TOKENIZER_PATH, MODEL_PATH):
+    """
+    Setting up model and tokenizer
+    """
+    with open(TOKENIZER_PATH, 'rb') as handle:
+        tokenizer = pickle.load(handle)
 
-model = keras.models.load_model(MODEL_PATH)
+    model = keras.models.load_model(MODEL_PATH)
 
-def get_key(value):
+    return tokenizer, model
+
+def _get_key(value):
+    """
+    (integer) -> string
+    Returning emotion associated with value
+    """
     dict={'joy':0,'anger':1,'love':2,'sadness':3,'fear':4,'surprise':5}
     for key, val in dict.items():
         if (val==value):
             return key
 
-def predict(sentence):
+def predict(sentence, model, tokenizer):
+    """
+    (string) -> emotion
+    Predicting emotion of provided text
+    """
     sentence_lst=[]
     sentence_lst.append(sentence)
     sentence_seq=tokenizer.texts_to_sequences(sentence_lst)
     sentence_padded=pad_sequences(sentence_seq,maxlen=80,padding='post')
-    ans=get_key(model.predict_classes(sentence_padded))
+    ans=_get_key(model.predict_classes(sentence_padded))
     return "The emotion predicted is", ans
 
-print(predict("I was totally surprised by that"))
+if __name__ == "__main__":
+    app.run(debug=True)
